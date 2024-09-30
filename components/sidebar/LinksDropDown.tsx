d6e3f3fd189a054516088bd81ca9e67c3164bcfb
Bunlong <bunlongly03@gmail.com>
2024-09-30 18:17:30 +0700

    LinksDropdown Component

--- components/sidebar/LinksDropDown.tsx
+++ components/sidebar/LinksDropDown.tsx
@@ -4,11 +4,14 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
-import { LuAlignLeft } from 'react-icons/lu'; 
+import { LuAlignLeft } from 'react-icons/lu';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 import UserIcon from './UserIcon';
 import { links } from '@/utils/links';
+import { SignedOut, SignedIn, SignInButton, SignUpButton } from '@clerk/nextjs';
+import { DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu';
+import SignOutLink from './SignOutLink';
 
 function LinksDropdown() {
   return (
@@ -20,19 +23,35 @@ function LinksDropdown() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-52' align='start' sideOffset={10}>
-        {links.map(link => {
-          return (
-            <DropdownMenuItem key={link.href}>
-              <Link
-                href={link.href}
-                className='capitalize flex items-center w-full'
-              >
-                {link.icon && <link.icon className='mr-2' />}{' '}
-                {link.label}
-              </Link>
-            </DropdownMenuItem>
-          );
-        })}
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode='modal'>
+              <button className='w-full text-left'>Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode='modal'>
+              <button className='w-full text-left'>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map(link => {
+            return (
+              <DropdownMenuItem key={link.href}>
+                <Link href={link.href} className='capitalize w-full flex'>
+                  {link.icon && <link.icon className='mr-2' />}
+                  {link.label}
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
